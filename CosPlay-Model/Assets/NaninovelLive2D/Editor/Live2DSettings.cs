ce456f3593a93a238f4f6f349e456ded47c72eee
WingNova <satr7h@gmail.com>
2019-06-27 13:35:25 -0700

    v2

---
+++ CosPlay-Model/Assets/NaninovelLive2D/Editor/Live2DSettings.cs
@@ -0,0 +1,22 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Naninovel
+{
+    public class Live2DSettings : ConfigurationSettings<Live2DConfiguration>
+    {
+        protected override string HelpUri => "guide/characters.html#live2d-characters";
+
+        private const string projectUrl = @"https://github.com/Elringus/NaninovelLive2D";
+
+        protected override void DrawConfigurationEditor ()
+        {
+            DrawDefaultEditor();
+
+            EditorGUILayout.Space();
+
+            if (GUILayout.Button("GitHub Project"))
+                Application.OpenURL(projectUrl);
+        }
+    }
+}
---
+++ CosPlay-Model/Assets/NaninovelLive2D/Runtime/Live2DConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Naninovel
+{
+    /// <summary>
+    /// Represents configuration data for <see cref="Live2DCharacter"/>.
+    /// </summary>
+    [System.Serializable]
+    public class Live2DConfiguration : Configuration
+    {
+        [Tooltip("The layer to use when rendering Live2D prefabs to render textures.")]
+        public int RenderLayer = 30;
+        [Tooltip("Camera prefab to use for rendering Live2D prefabs into render textures. Will use a default prefab when not provided.")]
+        public Camera CustomRenderCamera = null;
+        [Tooltip("Render camera ofsset from the rendered Live2D prefab.")]
+        public Vector3 CameraOffset = new Vector3(0, 0, -10);
+    }
+}
